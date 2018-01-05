868d5a586de1b8cdfc56a2af5e8b51baf51a9bce
Roman Filipovsky <roman.filipovsky@gmail.com>
2018-01-05 22:10:09 +0200

    Add files via upload

---
+++ frontend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '7vLhbMbt_3EEChFV_26isIwxn631GY22',
+        ],
+    ],
+];
